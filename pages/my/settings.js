b89c1e2ccdc27994f0fc9d91ae3d92421782e09c
han <691804633@qq.com>
2026-05-24 11:47:12 +0800

    feat(settings): add logout

--- pages/my/settings.js
+++ pages/my/settings.js
@@ -21,6 +21,22 @@ Page({
     });
   },
 
+  onLogout() {
+    wx.showModal({
+      title: '确认退出',
+      content: '确定退出当前账号吗？',
+      success: (res) => {
+        if (res.confirm) {
+          wx.removeStorageSync('openid');
+          getApp().globalData.openid = '';
+          getApp().globalData.userInfo = null;
+          wx.showToast({ title: '已退出', icon: 'success' });
+          setTimeout(() => wx.reLaunch({ url: '/pages/my/my' }), 1000);
+        }
+      }
+    });
+  },
+
   onAbout() {
     wx.showModal({
       title: '关于去哪钓',
